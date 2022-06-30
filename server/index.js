23e17edb440fbf806a96bdfa58233d714a6ef9db
Harryyoshi <75763081+HarryWang05@users.noreply.github.com>
2022-06-29 22:56:58 -0400

    Added read messages for discord bot

--- server/index.js
+++ server/index.js
@@ -4,27 +4,35 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
-const Discord = require("discord.js");
+const { Client, Intents } = require('discord.js');
 const apiPort = 5000
 
-const { Client, Intents } = require('discord.js');
-const { MessageEmbed } = require('discord.js');
+const { token } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-client.commands = new Discord.Collection();
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
 
 var example = {
-    example: 'Button Clicked'
+    example: ''
 };
+
+client.on('messageCreate', message => {
+    if (message.author.bot) return;
+    console.log(message.content)
+    example.example += message.content
+});
+
+client.login(token);
 
 app.get('/api', (req, res) => {
     res.send(JSON.stringify(example))
 })
-
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
